ff70fb61ce65dd53818d7f3707f1a64105af8593
cansu <cansuque5@gmail.com>
2023-04-26 12:49:46 +0300

    props,state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Col, Container, Row } from "reactstrap";
 
 function App() {
+  let productInfo = {title:"Product List"}
+  let categoryInfo = {title:"Category List"}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo}/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
